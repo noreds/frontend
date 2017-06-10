a819d41fa5e18a620d99bb1dd9fdce13c3185f69
jan <jh@datenfreunde.com>
2017-06-11 01:28:05 +0200

    nicer frontend

--- frontend.py
+++ frontend.py
@@ -6,9 +6,40 @@ from modules.get_all import clustered_items
 app = Flask(__name__)
 Bootstrap(app)
 
+import json
+
+# [{'score': 0.091796, 'tone_name': 'Anger', 'tone_id': 'anger'}, {'score': 0.052257, 'tone_name': 'Disgust', 'tone_id': 'disgust'}, {'score': 0.147788, 'tone_name': 'Fear', 'tone_id': 'fear'}, {'score': 0.046793, 'tone_name': 'Joy', 'tone_id': 'joy'}, {'score': 0.522373, 'tone_name': 'Sadness', 'tone_id': 'sadness'
+
+tone_mapping = {
+    'disgust': 'success',
+    'sadness': 'info',
+    'anger': 'danger',
+    'fear': 'warning',
+    'joy': 'primary'
+}
+
+
+for cluster in list(clustered_items.values()):
+    for item in cluster:
+        tone_categories = item['item']['item'].get('sentiment', {}).get('document_tone', {}).get('tone_categories', False)
+        if tone_categories:
+            #print(tone_categories)
+            for cat in tone_categories:
+                if cat['category_id'] == 'emotion_tone':
+                    item['tones'] = {}
+                    for tone in cat['tones']:
+                        item['tones'][tone_mapping[tone['tone_id']]] = int(tone['score'] * 10)
+
+# disgustr -> grün
+# sadness -> hellblau
+# anger -> rot
+# fear -> gelb
+# joy -> dunkelblau
+
 @app.route('/')
 def clusters():
-    return render_template('clusters.html', clusters=list(clustered_items.values()))
+    return render_template('clusters.html', clusters=list(clustered_items.values()), mapping=tone_mapping)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
