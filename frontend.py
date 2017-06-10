27f80ec20eb080699d0b3b9fea40139a475143bf
jan <jh@datenfreunde.com>
2017-06-10 21:07:10 +0200

    get items from mongodb

--- frontend.py
+++ frontend.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
+from modules.get_all import cluster_items
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('main.html')
+def clusters():
+    return render_template('clusters.html')
 
 
 if __name__ == '__main__':
---
+++ modules/get_all.py
@@ -0,0 +1,87 @@
+from pymongo import MongoClient
+import os
+import json
+
+client = MongoClient('mongodb://hans:noooz@52.59.186.178:27017/')
+# client = MongoClient('mongodb://hans:noooz@localhost:27017/')
+imported_db = client['news']
+imported_collection = imported_db['imported']
+
+imported_news = 'imported'
+
+all_tags_with_items = {}
+
+
+def query_all_items():
+    all_items = []
+    all_items_cursor = imported_collection.find({'item.text_en': {'$exists': True}})
+
+    for item in all_items_cursor:
+        all_items.append(item)
+
+    return all_items
+
+
+def cluster_items(all_items):
+    items = {}
+
+    for d in all_items:
+        item_tags = {}
+        for entity in d['item']['metadata']['entities']:
+            if entity['type'] != 'XMN-TAG':
+                entity_uri = entity['uri']
+                print(entity['label'])
+                print(entity['score'])
+
+                # tags appear more than once on an item - we count the first one here
+                if not item_tags.get(entity_uri, False):
+                    item_tags[entity_uri] = entity
+
+                    if not all_tags_with_items.get(entity_uri, False):
+                        all_tags_with_items[entity_uri] = {
+                            'label': entity['label'],
+                            'items': [d['item']['fullText']]}
+                    else:
+                        all_tags_with_items[entity_uri]['items'].append(d['item']['fullText'])
+
+                # append to item overview
+                if not items.get(d['_id'], False):
+                    items[d['_id']] = {'tags': item_tags,
+                                       'item': d}
+
+    ## clustering by tags
+
+    from numpy import array
+    from scipy.cluster.vq import vq, kmeans2, whiten
+
+    all_bin_tags = []
+
+    for item in items.values():
+        # print(item)
+        binary_tags = [1 if x in item['tags'] else 0 for x in all_tags_with_items.keys()]
+        # print(binary_tags)
+        print('items bin tags: %s' % len(binary_tags))
+        all_bin_tags.append(binary_tags)
+
+    print('len of all items: %s ' % len(all_bin_tags))
+
+    features = array(all_bin_tags)
+    whitened = whiten(features)
+
+    # book = array((whitened[0],whitened[2]))  # should be the k guess (fixed to 20 because was 2)
+
+    centroids, labels = kmeans2(whitened, 200)
+    clustered_items = {}
+
+    for num in range(len(labels)):
+        label = labels[num]
+        if not clustered_items.get(label, False):
+            clustered_items[label] = [item]
+        else:
+            clustered_items[label].append(item)
+
+    return clustered_items
+
+items = query_all_items()
+clustered_items = cluster_items(items)
+
